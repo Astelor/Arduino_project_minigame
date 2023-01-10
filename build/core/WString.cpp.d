a48cb37edaa812fc5d02dddd4142bc749c3b5df1
Astelor <astelor000@gmail.com>
2023-01-10 23:49:45 +0800

    delete ignored folders

--- build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-e:\!Github_Coding\Arduino_project_minigame\Arduino_project_minigame\build\core\WString.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WString.h
--- build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-e:\!Github_Coding\Arduino_project_minigame\Arduino_project_minigame\build\core\hooks.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\hooks.c
